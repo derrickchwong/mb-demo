53746797429d294aaa94c7e79a8ce1c79da033c9
Derrick Wong <derrick.chwong@gmail.com>
2023-12-12 12:09:59 +0800

    add remote web driver

---
+++ e2e-test/src/test/java/com/example/E2eRemoteTest.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class E2eRemoteTest {
+    
+
+    @Test
+    public void eightComponents() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("browserName", "chrome");
+
+        // WebDriver driver = new ChromeDriver();
+        WebDriver driver = new RemoteWebDriver(
+                new URL("http://admin:admin@34.110.186.13/wd/hub"),
+                desiredCapabilities);
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        driver.quit();
+    }
+}
